4ccd91473c771783d9fddab10cbea4def70ca435
jennafritz <jennafritz24@gmail.com>
2021-04-25 17:25:30 -0400

    commit tree 0117ab98d92200d2a3816a56ce4654ef8b8349dc

--- index.js
+++ index.js
@@ -20,3 +20,76 @@ let allWagesFor = function () {
 
     return payable
 }
+
+function createEmployeeRecord(personArray) {
+    let employeeRecordObj = {
+        firstName: personArray[0],
+        familyName: personArray[1],
+        title: personArray[2],
+        payPerHour: personArray[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+    return employeeRecordObj
+}
+
+function createEmployeeRecords(arrayOfPersonArrays) {
+    let arrayOfEmployeeObjects = arrayOfPersonArrays.map(createEmployeeRecord)
+    return arrayOfEmployeeObjects
+}
+
+let createTimeInEvent = function(dateStamp) {
+    let splitDate = dateStamp.split(" ")
+    this.timeInEvents = [...this.timeInEvents, {
+        type: "TimeIn",
+        hour: parseInt(splitDate[1], 10),
+        date: splitDate[0]
+    }]
+    return this
+}
+
+let createTimeOutEvent = function(dateStamp) {
+    let splitDate = dateStamp.split(" ")
+    this.timeOutEvents = [...this.timeOutEvents, {
+        type: "TimeOut",
+        hour: parseInt(splitDate[1], 10),
+        date: splitDate[0]
+    }]
+    return this
+}
+
+let hoursWorkedOnDate = function(date) {
+    let timeInObj = this.timeInEvents.find((timeInEventsObj) => {
+        return (timeInEventsObj.date === date)
+    })
+    let timeOutObj = this.timeOutEvents.find((timeOutEventsObj) => {
+        return (timeOutEventsObj.date === date)
+    })
+    let timeIn = timeInObj.hour
+    let timeOut = timeOutObj.hour
+    let hoursWorked = (timeOut - timeIn)/100
+    return hoursWorked 
+}
+
+let wagesEarnedOnDate = function(date) {
+    let hoursWorked = hoursWorkedOnDate.call(this, date)
+    let wagesEarned = (hoursWorked*this.payPerHour)
+    return wagesEarned
+}
+
+let findEmployeeByFirstName = function(arrayOfEmployeeRecords, searchName) {
+    let searchResult = arrayOfEmployeeRecords.find((employeeRecordObj) => {
+        return (employeeRecordObj.firstName === searchName)
+    })
+    return searchResult
+}
+
+let calculatePayroll = function(arrayOfEmployeeRecords) {
+    let arrayOfWages = []
+    arrayOfEmployeeRecords.forEach((employeeRecordObj) => {
+    let employeeWages = allWagesFor.call(employeeRecordObj)
+    arrayOfWages = [...arrayOfWages, employeeWages]
+    })
+    let totalWages = arrayOfWages.reduce((totalAmount, wages) => {return totalAmount + wages})
+    return totalWages
+    }
